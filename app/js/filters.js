7a2c838ab3717caa0f58e396ffda89f7cf5d085f
francois-travais <francois.travais@eniram.fi>
2015-01-25 16:48:41 +0200

    gift list: cities and types filters

--- app/js/filters.js
+++ app/js/filters.js
@@ -35,3 +35,49 @@ weddingFilters.filter('accomodationTypeFilter', ['MapIconMarker', function (MapI
         return matchs;
     };
 }]);
+
+
+function genericGiftFilter(array, expression, elment, markers, MapIconMarker) {
+    var matchs = [];
+    angular.forEach(array, function (el) {
+        var match = false;
+        for (var i = 0; i < expression.length; i++) {
+            if (expression[i].checked && ('' + el[elment]).indexOf(expression[i].display) > -1) {
+                matchs.push(el);
+                match = true;
+                break;
+            }
+        }
+        if (match) {
+            markers[el.id] = {
+                'lat': el.coordinates[0],
+                'lng': el.coordinates[1],
+                'title': el.name,
+                'message': el.name
+            };
+            if (el["icon"]) {
+                markers[el.id]["icon"] = MapIconMarker.icon(el.icon);
+            }
+        } else {
+            delete markers[el.id];
+        }
+    });
+    return matchs;
+}
+
+weddingFilters.filter('giftTypeFilter', ['MapIconMarker', function (MapIconMarker) {
+    return function (array, expression, markers) {
+        if (!angular.isArray(array)) return array;
+
+        return genericGiftFilter(array, expression, 'type', markers, MapIconMarker);
+    };
+}]);
+
+
+weddingFilters.filter('giftCityFilter', ['MapIconMarker', function (MapIconMarker) {
+    return function (array, expression, markers) {
+        if (!angular.isArray(array)) return array;
+
+        return genericGiftFilter(array, expression, 'city', markers, MapIconMarker);
+    };
+}]);
